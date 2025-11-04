29e2d37d47adbf02cc2728fbd2ccd20200e69c6a
Larissa Coelho <larisilvaco@hotmail.com>
2025-11-04 01:28:33 -0300

    chore: adjust build config for mac

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // MUITO IMPORTANTE: base './' deixa CSS/JS/IMAGENS com caminho relativo,
-  // funcionando no esquema file:// dentro do Electron
-  base: './',
-  build: {
-    outDir: "dist/web",
-    emptyOutDir: true
-  }
+  base: "./",
+  build: { outDir: "dist/web", emptyOutDir: true },
 });
